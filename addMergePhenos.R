eee5a301c10ac61477fd8355d615c0b883ab0039
JOHN LOVELL <johntlovell@gmail.com>
2015-06-17 12:14:37 -0500

    fixed bug

--- addMergePhenos.R
+++ addMergePhenos.R
@@ -1,5 +1,5 @@
 addMergePhenos<-function(cross, df){
-  id<-as.numeric(as.character(getid(cross.all)))
+  id<-as.numeric(as.character(getid(cross)))
   dat$id<-as.numeric(as.character(dat$id))
   dat<-dat[dat$id %in% id,]
   nodat<-id[!id %in% dat$id]
