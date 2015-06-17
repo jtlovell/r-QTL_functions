3cbec7069b17da43a6136cda4eb0a6bdbe845aef
JOHN LOVELL <johntlovell@gmail.com>
2015-06-17 08:10:30 -0500

    initial

---
+++ addMergePhenos.R
@@ -0,0 +1,14 @@
+addMergePhenos<-function(cross, df){
+  id<-as.numeric(as.character(getid(cross.all)))
+  dat$id<-as.numeric(as.character(dat$id))
+  dat<-dat[dat$id %in% id,]
+  nodat<-id[!id %in% dat$id]
+  nadf<-dat[1:length(nodat),]
+  nadf[, -which(colnames(nadf)=="id")]<-NA
+  nadf$id<-nodat
+  dat<-rbind(dat,nadf)
+  dat$id<-as.numeric(dat$id)
+  dat<-dat[order(dat$id),]
+  cross$pheno <- cbind(cross$pheno, dat)
+  return(cross)
+}
